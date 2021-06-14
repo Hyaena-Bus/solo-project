f2e58a5e1854ae3ef703f2011df6e408924738e8
Hyaena-Bus <shunkoba0701@gmail.com>
2021-06-14 14:07:55 +0900

    ゲーム完成

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,80 @@
 <template>
   <div id="app">
-    <div>{{states}}</div>
-    <table id="table">
-            <tr>
-                <td>{{states[0][0]}}</td>
-                <td>{{states[0][1]}}</td>
-                <td>{{states[0][2]}}</td>
-            </tr>
-            <tr>
-                <td>{{states[1][0]}}</td>
-                <td>{{states[1][1]}}</td>
-                <td>{{states[1][2]}}</td>
-            </tr>
-            <tr>
-                <td>{{states[2][0]}}</td>
-                <td>{{states[2][1]}}</td>
-                <td>{{states[2][2]}}</td>
-            </tr>
-        </table>
+    <div>{{state.text}}</div>
+    <div>{{`${state.playerId}の番です`}}</div>
+      <table id="table">
+        <tr>
+            <td @click="clicked(0,0)">{{state.states[0][0]}}</td>
+            <td @click="clicked(0,1)">{{state.states[0][1]}}</td>
+            <td @click="clicked(0,2)">{{state.states[0][2]}}</td>
+        </tr>
+        <tr>
+            <td @click="clicked(1,0)">{{state.states[1][0]}}</td>
+            <td @click="clicked(1,1)">{{state.states[1][1]}}</td>
+            <td @click="clicked(1,2)">{{state.states[1][2]}}</td>
+        </tr>
+        <tr>
+            <td @click="clicked(2,0)">{{state.states[2][0]}}</td>
+            <td @click="clicked(2,1)">{{state.states[2][1]}}</td>
+            <td @click="clicked(2,2)">{{state.states[2][2]}}</td>
+        </tr>
+      </table>
   </div>
 </template>
 
 <script>
+import { defineComponent, reactive } from '@vue/composition-api'
 
-
-export default {
-  name: 'App',
-  data() {
-    return {
+export default defineComponent({
+  setup() {
+    const state = reactive({
       states: [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
+        ["","",""],
+        ["","",""],
+        ["","",""],
       ],
-      playerId: 1,
+      playerId: true,
+      text: "",
+    });
+
+    const clicked = (row,column)=>{
+      if(state.states[row][column]){
+        console.log("そこには置けないよ")
+      }else{
+        state.playerId ? state.states[row][column]="○" : state.states[row][column]="×"
+        state.playerId = !state.playerId
+        if(winChecker()) state.text=`${!state.playerId}の勝ち！`
+      }
+    }
+
+    const winChecker = ()=>{
+      return rowChecker() || columnChecker() || diagonalChecker() 
+    }
+
+    const rowChecker = ()=>{
+      return (state.states[0][0] !== "" && state.states[0][0]===state.states[0][1] && state.states[0][1]===state.states[0][2]) 
+              || (state.states[1][0] !== "" && state.states[1][0]===state.states[1][1] && state.states[1][1]===state.states[1][2])
+              || (state.states[2][0] !== "" && state.states[2][0]===state.states[2][1] && state.states[2][1]===state.states[2][2])
+    }
+
+    const columnChecker = ()=>{
+      return (state.states[0][0] !== "" && state.states[0][0]===state.states[1][0] && state.states[1][0]===state.states[2][0])
+              || (state.states[0][1] !== "" && state.states[0][1]===state.states[1][1] && state.states[1][1]===state.states[2][1])
+              || ((state.states[0][2] !== "" && state.states[0][2]===state.states[1][2] && state.states[1][2]===state.states[2][2]))
+    }
+
+    const diagonalChecker = ()=>{
+      return (state.states[0][0] !== "" && state.states[0][0]===state.states[1][1] && state.states[1][1]===state.states[2][2])
+              || (state.states[0][2] !== "" && state.states[0][2]===state.states[1][1] && state.states[1][1]===state.states[2][0])
+    }
+
+    return {
+      state,
+      clicked,
     };
   },
-  components: {
-    
-  }
-}
+});
+
 </script>
 
 <style>
