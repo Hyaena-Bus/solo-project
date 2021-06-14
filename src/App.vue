e6c670b66ef0a3de87cffaa0b76742cf36b047fe
Hyaena-Bus <shunkoba0701@gmail.com>
2021-06-14 11:54:59 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{states}}</div>
+    <table id="table">
+            <tr>
+                <td>{{states[0][0]}}</td>
+                <td>{{states[0][1]}}</td>
+                <td>{{states[0][2]}}</td>
+            </tr>
+            <tr>
+                <td>{{states[1][0]}}</td>
+                <td>{{states[1][1]}}</td>
+                <td>{{states[1][2]}}</td>
+            </tr>
+            <tr>
+                <td>{{states[2][0]}}</td>
+                <td>{{states[2][1]}}</td>
+                <td>{{states[2][2]}}</td>
+            </tr>
+        </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      states: [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+      ],
+      playerId: 1,
+    };
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -25,4 +51,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#table {
+  margin:0 auto;
+  border-collapse: collapse;
+  border: 3px solid #ccc;
+}
+
+#table td {
+  border:1px solid #ccc;
+  height: 80px;
+  width: 80px;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 75px;
+  cursor: pointer;
+}
+
+
 </style>
