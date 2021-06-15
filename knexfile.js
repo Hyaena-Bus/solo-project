b4919776a447923a74a0cfa72318391487f96e1d
Hyaena-Bus <shunkoba0701@gmail.com>
2021-06-15 13:46:59 +0900

    edit config

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,9 @@ module.exports = {
       database: 'marubatu',
       user:     'postgres',
       password: 'test'
-    },
+    }
+    ||   `postgres://micbytzxnpgyso:d7f1043007d7b364cbef98aba3d13280d8d830ce6b34c74b9f874e2d3c82e2a8@ec2-3-233-7-12.compute-1.amazonaws.com:5432/marubatu`,
+
     pool: {
       min: 2,
       max: 10
